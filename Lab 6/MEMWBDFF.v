45680a9611f91566f7dbe60454bd7c5f62c40db2
Aryan Mehrotra <62621714+a-mehrotra@users.noreply.github.com>
2022-06-01 11:18:53 -0400

    Add files via upload

--- Lab 6/MEMWBDFF.v
+++ Lab 6/MEMWBDFF.v
@@ -7,18 +7,18 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module MEMWBDFF(daddrbus_in, databus_in, Dselect_in, clk, LW_in, daddrbus_out, databus_out, Dselect_out, LW_out);
+module MEMWBDFF(daddrbus_in, databus_in, Dselect_in, clk, SW_in, daddrbus_out, databus_out, Dselect_out, SW_out);
      //Declare inputs and outputs
     input[31:0] daddrbus_in, databus_in, Dselect_in;
-    input clk, LW_in;
+    input clk, SW_in;
     output reg[31:0] daddrbus_out, databus_out, Dselect_out;
-    output reg LW_out; 
+    output reg SW_out; 
     //Assign flip-flop values
     always @(posedge clk) begin
         daddrbus_out = daddrbus_in;
         databus_out = databus_in;
         Dselect_out = Dselect_in;
-        LW_out = LW_in;
+        SW_out = SW_in;
     end
 
 endmodule
--- Lab 6/cpu4.v
+++ Lab 6/cpu4.v
@@ -16,9 +16,9 @@ module cpu4(ibus, clk, daddrbus, databus);
     
     //Declare wires for intermediate values
     wire[31:0] decoderInput, Aselect, Bselect, rd_output, sign_ext_ID, regfile_out1, regfile_out2, mux1_out_ID, mux1_out_EX, sign_ext_EX, mux2_in_EX, ALUOutput, 
-        Dselect, abus, bbus, dbus, mux3_in1, mux3_in2;
+        Dselect, abus, bbus, dbus, mux3_in1, mux3_in2, mux1_out_MEM;
     wire[2:0] S_ID, S_EX;
-    wire Imm_ID, Cin_ID, LW_ID, Imm_EX, Cin_EX, LW_EX, Cout, V, LW_MEM, LW_WB;
+    wire Imm_ID, Cin_ID, SW_ID, Imm_EX, Cin_EX, SW_EX, Cout, V, SW_MEM, SW_WB;
     
     //IF/ID Flip-Flop Declaration
     DFF32 IFID_DFF(.DFFInput32(ibus), .DFFOutput32(decoderInput), .clk(clk));
@@ -29,7 +29,7 @@ module cpu4(ibus, clk, daddrbus, databus);
     //Set-up rd decoder and output to mux
     rd_decoder rd_decoder(.ibus(decoderInput), .mux_rd(rd_output));
     //Set-up opcode decoder and output to ID/EX DFF
-    opcode_decoder opcode_decoder(.ibus(decoderInput), .ImmID(Imm_ID), .SID(S_ID), .CinID(Cin_ID), .LWID(LW_ID));
+    opcode_decoder opcode_decoder(.ibus(decoderInput), .ImmID(Imm_ID), .SID(S_ID), .CinID(Cin_ID), .SWID(SW_ID));
     //Set-up mux to output Dselect to DFF
     mux32 mux1(.input1(rd_output), .input2(Bselect), .ImmID(Imm_ID), .mux_output(mux1_out_ID));
     //Set-up sign extension module
@@ -37,20 +37,22 @@ module cpu4(ibus, clk, daddrbus, databus);
     //Set-up register file 
     RegFile32x32 reg_file(.Aselect(Aselect), .Bselect(Bselect), .Dselect(Dselect), .abus(regfile_out1), .bbus(regfile_out2), .dbus(dbus), .clk(clk));
     //Set-up ID/EX D Flip-Flop
-    IDEXDFF IDEX_DFF(.reg_out1(regfile_out1), .reg_out2(regfile_out2), .ImmID(Imm_ID), .SID(S_ID), .CinID(Cin_ID), .LWID(LW_ID), .sign_ext_ID(sign_ext_ID), 
-                        .mux1_out_ID(mux1_out_ID), .clk(clk), .ALUInput1(abus), .mux2_in_EX(mux2_in_EX), .Sx(S_EX), .ImmEX(Imm_EX), .LWEX(LW_EX), .CinEX(Cin_EX), 
+    IDEXDFF IDEX_DFF(.reg_out1(regfile_out1), .reg_out2(regfile_out2), .ImmID(Imm_ID), .SID(S_ID), .CinID(Cin_ID), .SWID(SW_ID), .sign_ext_ID(sign_ext_ID), 
+                        .mux1_out_ID(mux1_out_ID), .clk(clk), .ALUInput1(abus), .mux2_in_EX(mux2_in_EX), .Sx(S_EX), .ImmEX(Imm_EX), .SWEX(SW_EX), .CinEX(Cin_EX), 
                         .mux1_out_EX(mux1_out_EX), .sign_ext_EX(sign_ext_EX));
     //Set-up mux to output bbus to ALU
     mux32 mux2(.input1(mux2_in_EX), .input2(sign_ext_EX), .ImmID(Imm_EX), .mux_output(bbus));
     //Set-up ALU
     alu32 alu(.d(ALUOutput), .Cout(Cout), .V(V), .a(abus), .b(bbus), .Cin(Cin_EX), .S(S_EX));
     //Set-up EX/MEM D Flip-Flop
-    EXMEMDFF EXMEM_DFF(.ALUOutput(ALUOutput), .mux1_out_EX(mux1_out_EX), .mux2_in_EX(mux2_in_EX), .clk(clk), .dbus(daddrbus), .Dselect(Dselect), 
-                       .LW_MEM(LW_MEM), .databus_in(databus));
+    EXMEMDFF EXMEM_DFF(.ALUOutput(ALUOutput), .mux1_out_EX(mux1_out_EX), .mux2_in_EX(mux2_in_EX), .SW_EX(SW_EX), .clk(clk), .dbus(daddrbus), .Dselect(mux1_out_MEM), 
+                       .SW_MEM(SW_MEM), .databus_in(databus));
+    //Set-up module for databus
+    data_mem databus_mod(.b_oper(mux2_in_EX), .databus(databus), .SW_MEM(SW_MEM));
     //Set-up MEM/WB D Flip-Flop
-    MEMWBDFF MEMWB_DFF(.daddrbus_in(daddrbus), .databus_in(databus), .Dselect_in(Dselect), .clk(clk), .LW_in(LW_MEM), .daddrbus_out(mux3_in2), .databus_out(mux3_in1),
-                       .Dselect_out(Dselect), .LW_out(LW_WB));
+    MEMWBDFF MEMWB_DFF(.daddrbus_in(daddrbus), .databus_in(databus), .Dselect_in(mux1_out_MEM), .clk(clk), .SW_in(SW_MEM), .daddrbus_out(mux3_in2), .databus_out(mux3_in1),
+                       .Dselect_out(Dselect), .SW_out(SW_WB));
     //Set-up mux to output to dbus
-    mux32 mux3(.input1(mux3_in1), .input2(mux3_in2), .ImmID(LW_WB), .mux_output(dbus));
+    mux32 mux3(.input1(mux3_in2), .input2(mux3_in1), .ImmID(SW_WB), .mux_output(dbus));
 
 endmodule
